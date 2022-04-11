a4835ea5242460fd6a17fa73b991cee103122b16
m.arifin46 <muhamadarifin428@gmail.com>
2022-04-12 00:06:58 +0700

    Add files via upload

---
+++ src/components/common/EmptyList/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const EmptyList = () => (
+  <div className='emptyList-wrap'>
+    <img src='#' alt='empty' />
+  </div>
+);
+
+export default EmptyList;
---
+++ src/config/data.js
@@ -0,0 +1,19 @@
+export const blogList = [
+  {
+    "id": 1,
+    "title": "Hello world",
+    "content": "Hello world dang dang",
+    "published_at": "2021-10-13T05:07:57.208Z",
+    "created_at": "2021-10-13T05:07:52.434Z",
+    "updated_at": "2021-10-13T05:07:57.228Z"
+  },
+  {
+    "id": 2,
+    "title": "Hello world",
+    "content": "Hello world dang dang",
+    "published_at": "2021-10-13T05:07:57.208Z",
+    "created_at": "2021-10-13T05:07:52.434Z",
+    "updated_at": "2021-10-13T05:07:57.228Z"
+  },
+  
+];
